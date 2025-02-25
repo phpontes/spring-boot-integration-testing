de8ad65dca9ae2a6acc77cf75b5b5c36ed1a3869
phpontes <ppontes95@hotmail.com>
2025-02-25 15:56:48 -0300

    First integration test for JPA repository

---
+++ src/main/java/com/linkedin/student_service/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.linkedin.student_service;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    Student getStudentByName(String name);
+
+}
